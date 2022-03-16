a5d04a4d1ece445821cbf5b9cc523c19c1d91a44
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-16 15:04:31 +0100

    inizio stesura esercizio 2 e 3

---
+++ Snack 2/main.js
@@ -0,0 +1,9 @@
+let nomi = ["Luca ", "Paolo ", "Daniele ", "Marco ", "Michele "]
+
+let cognomi = ["Rossi","Ferrari","Russo","Bianchi","Romano"]
+
+
+for (i=0; i < 10; i++) {
+    let nomeCompleto = nomi[i] + cognomi[i]
+    console.log (nomeCompleto)
+}
---
+++ Snack 3/main.js
@@ -0,0 +1,9 @@
+let numeri = ["8", "5", "7", "6", "3"]
+
+let somma = 0;
+
+for (i=0; i < numeri.length; i = i + 2) {
+    console.log(numeri[i])
+    somma = somma + numeri[i]
+    console.log(somma)
+}
