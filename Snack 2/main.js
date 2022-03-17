34da94cf4535b60eb373cc8be6d37f0d2e8e5163
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-17 15:44:38 +0100

    stesura finale snack 2

--- Snack 2/main.js
+++ Snack 2/main.js
@@ -2,10 +2,14 @@ let nomi = ["Luca ", "Paolo ", "Daniele ", "Marco ", "Michele "]
 
 let cognomi = ["Rossi","Ferrari","Russo","Bianchi","Romano"]
 
-for (i=0; i < nomi.length; i++) {
-    let y = Math.floor(Math.random() * (cognomi.length))
-    let nome = nomi[i]
-    let cognome = cognomi[y]
-}
+let lista = []
 
-document.getElementById("risultato").innerHTML = nome + cognome
+for (i=0; i < nomi.length; i++) {
+    const numeroRandomNomi = Math.floor(Math.random() * (nomi.length))
+    console.log (`Numero random nomi: ${numeroRandomNomi}`)
+    const numeroRandomCognomi = Math.floor(Math.random() * (cognomi.length))
+    console.log (`Numero random nomi: ${numeroRandomCognomi}`)
+
+    const nomeCognome = ` ${nomi[numeroRandomNomi]} ${cognomi[numeroRandomCognomi]}`
+    document.getElementById("risultato").innerHTML = nomeCognome
+}
